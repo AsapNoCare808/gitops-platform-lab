bf71792115556b5f6c1e5da07156c5d1be9f1c89
AsapNoCare808 <toris.maxime@gmail.com>
2026-05-04 01:35:46 +0200

    fix outputs

--- terraform/infrastructure/outputs.tf
+++ terraform/infrastructure/outputs.tf
@@ -3,9 +3,9 @@ output "Public_IP_EC2_Instance" {
 }
 
 output "vpc_id" {
-  value = aws_vpc.Infra_GitOps.id
+  value = aws_vpc.main.id
 }
 
 output "subnet_id" {
-  value = aws_subnet.Infra_GitOps.id
+  value = aws_subnet.main.id
 }
